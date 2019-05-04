9680da1b16d310a5342aa1c40a30e34942c798f1
root <root@ip-172-31-29-4.eu-west-1.compute.internal>
2019-05-04 00:25:49 +0000

    create bucket

---
+++ Dev /bucket.tf
@@ -0,0 +1,10 @@
+resource "aws_s3_bucket" "newbucket" {
+  bucket = "terraform-january-dilfuza2"
+
+  tags {
+     Name = "terraform-december-dilfuza2" 
+     Env  = "dev" 
+     Dept = "IT" 
+     Created_by = "dilfuza"
+  }
+}
